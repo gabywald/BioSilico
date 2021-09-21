7dc5bfc7e638cba6584e57434eec28b0f5873288
gabywald <30340425+gabywald@users.noreply.github.com>
2021-09-21 11:45:53 +0200

    [biosilico][genetics][builder]Notes for complex builders (start).

---
+++ biosilico-biosilico/src/main/java/gabywald/biosilico/genetics/builders/complex/PathwayBuilder.java
@@ -0,0 +1,25 @@
+package gabywald.biosilico.genetics.builders.complex;
+
+/**
+ * Complex builder for Pathways. 
+ * @author Gabriel Chandesris (2021)
+ */
+public class PathwayBuilder {
+	
+	// XXX NOTE 20210921 : aim of this class is to build several gene at the same time 
+	// // // - BiochemicalReaction (for base metabolic pathway)
+	// // // - EmitterReceptor (Input / Output) if needed in definition
+	// // // - StimulusDecision (Input / Output) if needed in definition
+	// TO BE DONE SEPARATELY : 
+	// // // - InitialConcentration (if any need)
+	// // // - Brain / BrainLobe / Instinct (if any need) : another specific complex builder for that purpose
+	// // // - "Complete Genome Builder" by type of Organism (cf . gabywald.biosilico.model.enums.AgentType)
+	
+	// How It work (or should do), definition to review (!?)
+	// // // - output : a list of Gene's instances (BR / ER / SD)
+	// // // - input : a set of chemicals (or indexes) of starts, intermediates and destinations with ratio / coefficients
+	// // // - ... 
+	
+	// 
+	
+}
