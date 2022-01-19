87ecf7951aa68f5fad88aea9f3fbfe04c9806dc9
gabywald <30340425+gabywald@users.noreply.github.com>
2022-01-19 17:26:28 +0100

    [biosilico][anthill][genetics]To analyse further what to change ! (2)

--- biosilico-anthill/src/test/java/gabywald/biosilico/anthill/computations/DataExporterAndViewAnalysis.java
+++ biosilico-anthill/src/test/java/gabywald/biosilico/anthill/computations/DataExporterAndViewAnalysis.java
@@ -118,4 +118,120 @@ class DataExporterAndViewAnalysis {
 		}
 	}
 	
+	@Test
+	void testAntExportChemicalsOnSteps() {
+		
+		Ant testAnt = new Ant();
+		Assertions.assertNotNull( testAnt );
+		Assertions.assertEquals(0, testAnt.getGenome().size());
+		
+		testAnt.setRank("Rank Test");
+		testAnt.setNameCommon("Test Starting Ant");
+		testAnt.setNameBiosilico("PlAntHill Ant Example");
+		testAnt.setDivision("TESTS");
+		
+		testAnt.setGenome( AntHillExampleHelper.loadingAntGenome() );
+		
+		// ***** test with a World and WorldCase
+		World2D w		= new World2D(1, 1);
+		World2DCase wc	= w.getWorldCase(0,  0);
+		Assertions.assertNotNull( wc );
+		
+		testAnt.setCurrentWorldCase( wc );
+		// ***** Put DiOxygen && H2O && Energy in local WorldCase !!
+		wc.getChemicals().setVariable(SomeChemicals.DIOXYGEN.getIndex(), 	100);
+		wc.getChemicals().setVariable(SomeChemicals.WATER.getIndex(), 		100);
+		EnergySource es = new EnergySource();
+		wc.addAgent( es );
+		
+		StringBuilder sbExportData = new StringBuilder();
+		
+		// ***** one execution in this context
+		IntStream.range(0, 200).forEach( i -> {
+			w.execution();
+			testAnt.cyclePlusPlus(); // Aging organism
+			sbExportData.append( "STEP [")
+						.append( testAnt.getChemicals().getVariable(StateType.AGING.getIndex()) ) 
+						.append("]\n");
+			sbExportData.append( testAnt.getChemicals().toString() ).append( "*****\n" );
+			sbExportData.append( wc.getChemicals().toString() ).append( "*****\n" );
+		});
+		
+		File statisticsData = new File( "src/test/resources/" + "ExportAntStatistics.txt" );
+		statisticsData.setChamps( sbExportData.toString().split("\n") );
+		try {
+			statisticsData.printFile();
+		} catch (DataException e) {
+			// e.printStackTrace();
+			String msg = "Cannot write {" + statisticsData.getFileName() + "} ; DataException {" + e.getMessage() + "}";
+			Logger.printlnLog(LoggerLevel.LL_ERROR, msg);
+		}
+	}
+	
+	@Test
+	void testAntAndPlantExportChemicalsOnSteps() {
+		
+		Ant testAnt = new Ant();
+		Assertions.assertNotNull( testAnt );
+		Assertions.assertEquals(0, testAnt.getGenome().size());
+		
+		testAnt.setRank("Rank Test");
+		testAnt.setNameCommon("Test Starting Ant");
+		testAnt.setNameBiosilico("PlAntHill Ant Example");
+		testAnt.setDivision("TESTS");
+		
+		testAnt.setGenome( AntHillExampleHelper.loadingAntGenome() );
+		
+		Plant testPlant = new Plant();
+		Assertions.assertNotNull( testPlant );
+		Assertions.assertEquals(0, testPlant.getGenome().size());
+		
+		testPlant.setRank("Rank Test");
+		testPlant.setNameCommon("Test Starting Plant");
+		testPlant.setNameBiosilico("PlAntHill Plant Example");
+		testPlant.setDivision("TESTS");
+		
+		testPlant.setGenome( AntHillExampleHelper.loadingPlantGenome() );
+		
+		// ***** test with a World and WorldCase
+		World2D w		= new World2D(1, 1);
+		World2DCase wc	= w.getWorldCase(0,  0);
+		Assertions.assertNotNull( wc );
+		
+		testAnt.setCurrentWorldCase( wc );
+		testPlant.setCurrentWorldCase( wc );
+		// ***** Put DiOxygen && H2O && Energy in local WorldCase !!
+		wc.getChemicals().setVariable(SomeChemicals.DIOXYGEN.getIndex(), 	100);
+		wc.getChemicals().setVariable(SomeChemicals.WATER.getIndex(), 		100);
+		EnergySource es = new EnergySource();
+		wc.addAgent( es );
+		
+		StringBuilder sbExportData = new StringBuilder();
+		
+		// ***** one execution in this context
+		IntStream.range(0, 200).forEach( i -> {
+			w.execution();
+			testAnt.cyclePlusPlus(); // Aging organism
+			testPlant.cyclePlusPlus(); // Aging organism
+			sbExportData.append( "STEP [")
+						.append( testAnt.getChemicals().getVariable(StateType.AGING.getIndex()) ) 
+						.append("][")
+						.append( testPlant.getChemicals().getVariable(StateType.AGING.getIndex()) ) 
+						.append("]\n");
+			sbExportData.append( testAnt.getChemicals().toString() ).append( "*****\n" );
+			sbExportData.append( testPlant.getChemicals().toString() ).append( "*****\n" );
+			sbExportData.append( wc.getChemicals().toString() ).append( "*****\n" );
+		});
+		
+		File statisticsData = new File( "src/test/resources/" + "ExportAntAndPlantStatistics.txt" );
+		statisticsData.setChamps( sbExportData.toString().split("\n") );
+		try {
+			statisticsData.printFile();
+		} catch (DataException e) {
+			// e.printStackTrace();
+			String msg = "Cannot write {" + statisticsData.getFileName() + "} ; DataException {" + e.getMessage() + "}";
+			Logger.printlnLog(LoggerLevel.LL_ERROR, msg);
+		}
+	}
+	
 }
