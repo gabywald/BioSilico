1a6ba55908236dd8e74d0e148b2f4c042fcf9f4b
gabywald <30340425+gabywald@users.noreply.github.com>
2025-08-25 18:16:06 +0200

    [biosilico][crypto]adding some unit tests

--- biosilico-crypto/src/test/java/gabywald/crypto/model/tests/GeneticTranslatorTests.java
+++ biosilico-crypto/src/test/java/gabywald/crypto/model/tests/GeneticTranslatorTests.java
@@ -1,7 +1,9 @@
 package gabywald.crypto.model.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import gabywald.crypto.data.BiologicalUtils;
 import gabywald.crypto.model.GeneticTranslator;
 
 /**
@@ -14,9 +16,58 @@ class GeneticTranslatorTests {
 	void testGeneticTranslatorSomeTests() {
 		// TODO 'DP builder' for Genetic Translator
 		GeneticTranslator gt01REF = new GeneticTranslator(1, true);
-		
 		System.out.println(gt01REF.toString());
 	}
+	
+	@Test
+	void testGeneticTranslatorMoreTests01() {
+		// TODO 'DP builder' for Genetic Translator
+		GeneticTranslator forFileContent = BiologicalUtils.getGenericCrypto(0);
+		System.out.println(forFileContent.toString());
+		
+		String toEncrypt = "test test2 test3";
+		String encodedFileContent = forFileContent.encode(toEncrypt, 0);
+		System.out.println("[" + encodedFileContent + "]");
+		System.out.println("[" + encodedFileContent.substring(3, encodedFileContent.length()-3) + "]");
+		
+		// Assertions.assertEquals("tttactcacgccctatctcaagaactcacgccctatctcaatagagaactcacgccctatctcaatattttg", encodedFileContent);
+		
+		// Assertions.assertEquals("ttgtctcacgccctatctcaagaactcacgccctatctcaatagagaactcacgccctatctcaatattttt", forFileContent.encode(toEncrypt, 0));
+		// Assertions.assertEquals("ttgtctcacgccctatctcaagaactcacgccctatctcaatagagaactcacgccctatctcaatattttg", forFileContent.encode(toEncrypt, 1));
+		
+		// Assertions.assertEquals("tctcacgccctatctcaagaactcacgccctatctcaatagagaactcacgccctatctcaatatt", forFileContent.encode(toEncrypt, 0).substring(3, encodedFileContent.length()-3));
+		// Assertions.assertEquals("tctcacgccctatctcaagaactcacgccctatctcaatagagaactcacgccctatctcaatatt", forFileContent.encode(toEncrypt, 1).substring(3, encodedFileContent.length()-3));
+				
+		// String isDecrypted00 = forFileContent.decode(encodedFileContent.substring(3, encodedFileContent.length()-3), 0, 0);
+		String isDecrypted01 = forFileContent.decodeWithStartStopCodons(encodedFileContent, 0, 0);
+		System.out.println("[" + isDecrypted01 + "] <= [" + toEncrypt + "]");
+		Assertions.assertEquals(isDecrypted01, toEncrypt);
+		
+		System.out.println();
+	}
+	
+	@Test
+	void testGeneticTranslatorMoreTests02() {
+		// TODO 'DP builder' for Genetic Translator
+		GeneticTranslator forPathDirName = BiologicalUtils.getGenericCrypto(1);
+		System.out.println(forPathDirName.toString());
+		String toEncrypt = "/home/user/file.txt";
+		String encodedFilePath = forPathDirName.encode(toEncrypt, 1);
+		System.out.println("[" + encodedFilePath + "]" + "\n");
+
+		// Assertions.assertEquals("PWCHDEHTIINMTHTRTYCMCNHHICNSPHTMWACPDLHPYY", encodedFilePath);
+		
+		// Assertions.assertEquals("PWCHDEHTIINMTHTRTYCMCNHHICNSPHTMWACPDLHPYY", forPathDirName.encode("/home/user/file.txt", 0));
+		// Assertions.assertEquals("PWCHDEHTIINMTHTRTYCMCNHHICNSPHTMWACPDLHPYY", forPathDirName.encode("/home/user/file.txt", 1));
+		
+		// String isDecrypted00 = forPathDirName.decode(encodedFilePath.substring(3, encodedFilePath.length()-3), 0, 0);
+		String isDecrypted01 = forPathDirName.decodeWithStartStopCodons(encodedFilePath, 0, 0);
+		System.out.println("[" + isDecrypted01 + "] <= [" + toEncrypt + "]");
+		Assertions.assertEquals(isDecrypted01, toEncrypt);
+	
+		System.out.println();
+	}
+
 
 	
 	// TODO complete these tests !! GeneticTranslatorTests
